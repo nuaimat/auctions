6b4cc038f9fd7ea4a16447f0419f7781d5e2d784
Mo Nuaimat <nuaimat@gmail.com>
2017-06-19 11:58:35 -0500

    first commit

---
+++ src/main/java/edu/mum/cs544/auctions/Application.java
@@ -0,0 +1,11 @@
+package edu.mum.cs544.auctions;
+
+/**
+ * Created by Mo Nuaimat <nuaimat@gmail.com>
+ * nuaimat on 6/19/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello! World");
+    }
+}
---
+++ src/test/java/edu/mum/cs544/signup/SignupControllerTest.java
@@ -0,0 +1,26 @@
+package edu.mum.cs544.signup;
+/*
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import edu.mum.cs544.config.WebSecurityConfigurationAware;
+
+public class SignupControllerTest extends WebSecurityConfigurationAware {
+    @Test
+    public void displaysSignupForm() throws Exception {
+        mockMvc.perform(get("/signup"))
+                .andExpect(model().attributeExists("signupForm"))
+                .andExpect(view().name("signup/signup"))
+                .andExpect(content().string(
+                        allOf(
+                                containsString("<title>Signup</title>"),
+                                containsString("<legend>Please Sign Up</legend>")
+                        ))
+                );
+    }
+}*/
