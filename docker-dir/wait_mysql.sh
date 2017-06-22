94d2d4b9a0d079b5f70b6e1787d15538694bbfd8
Mo Nuaimat <nuaimat@gmail.com>
2017-06-22 02:05:11 -0500

    adding docker but not working fine yet

--- docker-dir/wait_mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OUTPUT="";
-while [ `echo $OUTPUT | grep -c somestring` = 0 ]; do
-  OUTPUT=`mysql -u root -p'root' -h database -e "show databases" | grep "auctions"`;
-  sleep 2
-  echo "Waiting for mysql .."
-done
